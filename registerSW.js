dcf753934851873c9b1d9e60979f1ed927d607df
Mustafa <alsmani2702019@gmail.com>
2026-04-09 21:08:02 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hybrid-system-pwa/sw.js', { scope: '/hybrid-system-pwa/' })})}
